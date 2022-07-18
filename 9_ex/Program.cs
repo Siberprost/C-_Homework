f51b01c481ffe80ba081090e074c862a72f6db7d
Siberprost <Skripnichencko.nikita@yandex.ru>
2022-07-18 21:54:08 +0300

    added 9_ex

---
+++ 9_ex/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("введите пятизначно число");
+string num = Console.ReadLine();
+if (num[0] == num[4] && num[1]== num[3])
+   Console.WriteLine("да полиндром");
+   else Console.WriteLine("не полиндром");
